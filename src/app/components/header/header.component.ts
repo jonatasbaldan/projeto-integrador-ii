d451849aec2cc124d5dbb2aed4ef0d9ef30c3c0a
Jonatas Baldan <jonatasbzc@gmail.com>
2023-10-20 20:32:09 -0300

    Implementado o layout responsivo do Header da home page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css', '../section.css']
 })
+
 export class HeaderComponent {
 
 }
