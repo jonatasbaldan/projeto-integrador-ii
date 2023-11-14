57dcb94e348cf42022757a6b14b75c0333d24897
jonatasbaldan <jonatasbzc@gmail.com>
2023-11-14 20:54:29 -0300

    adicionado rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { MakeScheduleComponent } from './components/pages/make-schedule/make-schedule.component';
+import { SchedulesComponent } from './components/pages/schedules/schedules.component';
+import { VaccinesComponent } from './components/pages/vaccines/vaccines.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'agendar', component: MakeScheduleComponent, pathMatch: 'prefix' },
+  { path: 'agendamentos', component: SchedulesComponent, pathMatch: 'prefix' },
+  { path: 'vacinas', component: VaccinesComponent, pathMatch: 'prefix' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
