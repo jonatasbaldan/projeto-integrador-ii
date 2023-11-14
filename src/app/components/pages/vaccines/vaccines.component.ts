c1ceb866f025adde2c5a5a0556ace455c79c56a4
jonatasbaldan <jonatasbzc@gmail.com>
2023-11-14 11:32:59 -0300

    Adicionado layout responsivo

--- src/app/components/pages/vaccines/vaccines.component.ts
+++ src/app/components/pages/vaccines/vaccines.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-vaccines',
   templateUrl: './vaccines.component.html',
-  styleUrls: ['./vaccines.component.css', '../../section.css']
+  styleUrls: ['./vaccines.component.css', '../../section.css'],
 })
-
 export class VaccinesComponent {
+  isDesktop = false;
+  isMobile = false;
 
+  @HostListener('window:resize', ['$event'])
+  ngOnInit(): void {
+    this.isDesktop = window.innerWidth >= 768;
+    this.isMobile = window.innerWidth < 768;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isDesktop = window.innerWidth >= 768;
+    this.isMobile = window.innerWidth < 768;
+  }
 }
